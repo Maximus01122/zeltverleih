ea5bb50525b9622b56d6f5dbd4c311e3dd92fe00
Maximus01122 <78544349+Maximus01122@users.noreply.github.com>
2024-09-16 23:05:49 +0200

    Delete src directory

--- src/main/java/de/zeltverleih/repository/LoadingFeeRepository.java
+++
@@ -1,7 +0,0 @@
-package de.zeltverleih.repository;
-
-import de.zeltverleih.model.datenbank.LoadingFee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoadingFeeRepository extends JpaRepository<LoadingFee, Long> {
-}
--- src/main/java/de/zeltverleih/repository/SetupServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package de.zeltverleih.repository;
-
-import de.zeltverleih.model.datenbank.SetupService;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface SetupServiceRepository extends JpaRepository<SetupService, Long> {
-}
