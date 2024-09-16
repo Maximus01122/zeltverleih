b51b48d949d57940766379b3dbb5e6caca683645
Maximus01122 <78544349+Maximus01122@users.noreply.github.com>
2024-09-16 23:05:32 +0200

    Delete main directory

--- main/java/de/zeltverleih/repository/AddressRepository.java
+++
@@ -1,8 +0,0 @@
-package de.zeltverleih.repository;
-
-import de.zeltverleih.model.datenbank.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
-
